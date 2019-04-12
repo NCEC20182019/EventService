eafe67adbb5b13478e31fb996b0b7f2bc64ca9c1
ArtemSglv <stiw00@bk.ru>
2019-04-12 15:14:20 +0300

    - fix deploy.sh for using pseudo-terminal

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ scp -i ~/.ssh/id_rsa \
     target/EventService-1.0.jar \
     $REMOTEUSER@$REMOTEHOST:$REMOTEAPPDIR
 echo 'Restart server...'
-ssh -i ~/.ssh/id_rsa $REMOTEUSER@$REMOTEHOST << EOF
+ssh -tt -i ~/.ssh/id_rsa $REMOTEUSER@$REMOTEHOST << EOF
 
 pgrep java | xargs kill -9
 nohup java -jar EventService-1.0.jar > log.txt &
