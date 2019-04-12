d4af9280212fd9543c8b583384c361850331937d
ArtemSglv <stiw00@bk.ru>
2019-04-12 22:18:52 +0300

    - change .travis.yml and deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,5 @@ echo 'Copy files...'
 scp -i ~/.ssh/id_rsa \
     target/EventService-1.0.jar \
     $REMOTEUSER@$REMOTEHOST:$REMOTEAPPDIR
-echo 'Restart server...'
-ssh -tt -i ~/.ssh/id_rsa $REMOTEUSER@$REMOTEHOST << EOF
-
-pgrep java | xargs kill -9
-nohup java -jar EventService-1.0.jar > log.txt &
-
-EOF
 
 echo 'Bye'
