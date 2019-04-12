5c61e7410dc0ee9ad0a43c4a4d0f66c145027732
ArtemSglv <stiw00@bk.ru>
2019-04-12 14:56:25 +0300

    - fix deploy.sh 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ scp -i ~/.ssh/id_rsa \
     target/EventService-1.0.jar \
     $REMOTEUSER@$REMOTEHOST:$REMOTEAPPDIR
 echo 'Restart server...'
-ssh -i ~/.ssh/id_rsa $REMOTE_USER@$REMOTE_HOST << EOF
+ssh -i ~/.ssh/id_rsa $REMOTEUSER@$REMOTEHOST << EOF
 
 pgrep java | xargs kill -9
 nohup java -jar EventService-1.0.jar > log.txt &
